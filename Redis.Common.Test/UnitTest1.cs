893939870555103e0b54655d538d3b4ee86c837f
Aaron.Wu <wuxiaohui50@163.com>
2017-05-31 17:13:51 +0800

    提交Redis类库

---
+++ Redis.Common.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Redis.Common.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static Redis.RedisHelper redisHelper = new Redis.RedisHelper(0);
+        [TestMethod]
+        public void Add()
+        {
+          
+            bool isSet = redisHelper.StringSet("key-1", "100", new TimeSpan(0, 5, 0));
+            Assert.IsTrue(isSet);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            string str = redisHelper.StringGet("key-1");
+            Assert.IsNotNull(str);
+        }
+    }
+}
